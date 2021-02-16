9cf76b63dba1b4d41c3abeba154a0be6c0054637
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-02-16 10:39:05 +0900

    baekjoon : 4153번 (직각삼각형)

---
+++ baekjoon/백준_4153.py
@@ -0,0 +1,18 @@
+### 백준 4153번 직각삼각형 ###
+
+import sys
+input = sys.stdin.readline
+
+while 1:
+    a = list(map(int, input().split()))
+    if a[0] == 0 and a[1] == 0 and a[2] == 0:
+        break
+
+    a.sort(reverse=False)
+
+    if (a[0] ** 2) + (a[1] ** 2) == (a[2] ** 2):
+        print("right")
+    else:
+        print("wrong")
+        
+
