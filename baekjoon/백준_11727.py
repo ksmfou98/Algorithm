a348407a23bfd77db0c941246d6c50cbcba92b18
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-03-30 13:29:34 +0900

    Add files via upload

---
+++ baekjoon/백준_11727.py
@@ -0,0 +1,13 @@
+# 백준 11727번 : 2 x n 타일링 2
+
+n = int(input())
+
+arr = [0] * 1001
+
+arr[1] = 1
+arr[2] = 3
+
+for i in range(3, n + 1):
+    arr[i] = (arr[i - 2] + arr[i - 2] + arr[i - 1]) % 10007
+
+print(arr[n])
