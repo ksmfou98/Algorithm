2b77eaa585255fe84443ca3620bdc4342559205c
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-04 21:09:40 +0900

    Most Common Word

---
+++ leetcode/Most Common Word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [
+            word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split()
+            if word not in banned
+        ]
+
+        counts = collections.Counter(words)
+        return coutns.most_common(1)[0][0]
