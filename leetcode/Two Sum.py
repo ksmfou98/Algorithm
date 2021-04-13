2841c2d6039bdb2de6a88dfa0fa50aeb78bbbee2
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-13 20:24:36 +0900

    Two Sum

---
+++ leetcode/Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, n in enumerate(nums):
+                complement = target - n
+
+                if complement in nums[i+1: ]:
+
+                    return nums.index(n), nums[i+1:].index(complement) + (i+1)
+        
