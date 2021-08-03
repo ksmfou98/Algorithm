5d2d973691f639bff20d01cf23dd7eb757588634
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-08-03 12:31:38 +0900

    programmers : 숫자 문자열과 영단어

--- programmers/숫자 문자열과 영단어.py
+++ programmers/숫자 문자열과 영단어.py
@@ -1,18 +1,16 @@
 def solution(s):
-    number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    result = []
-    s = list(s)
-    i = 0
-    while len(s) > 0:
-        
-        if s[0].isdigit():
-            result.append(s[0])
-            del s[0]
-            
-        if "".join(s[0:i]) in number:
-            result.append(str(number.index("".join(s[0:i]))))
-            del s[0:i]
-            i = 0
-        else:
-            i += 1
-    return int("".join(result))
+    words = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+    return int(s)
